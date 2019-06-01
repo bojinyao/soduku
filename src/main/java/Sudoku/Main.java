419bdd71ccec7bcc73e2652f407d3c1bfe9fab39
Bojin Yao <bojinyao@berkeley.edu>
2019-05-31 19:34:45 -0700

    started with Main.java and Solver.java

--- src/main/java/Sudoku/Main.java
+++ src/main/java/Sudoku/Main.java
@@ -2,11 +2,34 @@ package Sudoku;
 
 
 /**
- * Sudoku solver class
+ * Program entry point
+ *
+ *
+ * User will need to enter appropriate parameters
+ *
+ * arg[1] (int): number of entries. 3x3 board have 9 entries
+ * arg[2] (flag):
+ *      -a  find all valid solutions
+ *      -f  find first valid solution
+ * arg[3+] (int): actual entries of the board
+ *  in the order of top to bottom, left to right
  */
 public class Main {
 
+    /** using a Thread Pool of 10 threads */
+    private static final int POOLSIZE = 10;
+
     public static void Main(String... args) {
+
+        int[] board = {};
+        boolean findAll;
+        /* parsing input */
+
+        Solver solver = new Solver(board);
+        for (int i = 0; i < POOLSIZE; i++) {
+            Thread curr = new Thread(solver);
+            curr.start();
+        }
 
     }
 }
--- src/main/java/Sudoku/Solver.java
+++ src/main/java/Sudoku/Solver.java
@@ -17,6 +17,7 @@ public class Solver implements Runnable {
     /** Stores each variance of SquareBoards to be solved */
     private Stack<SquareBoard> jobs;
 
+
     public Solver(SquareBoard board) {
         this.board = board;
         this.size = board.getSize();
@@ -24,7 +25,35 @@ public class Solver implements Runnable {
     }
 
 
+    /**
+     * Add next board to be solved.
+     * @param board
+     */
+    private synchronized void addJob(SquareBoard board) {
+        jobs.push(board);
+    }
+
+    /**
+     * pop from
+     * @return next job to run
+     */
+    private synchronized SquareBoard nextJob() {
+        return jobs.pop();
+    }
+
+
+    /**
+     * A worker's job.
+     * The "solve" method.
+     */
     public void run() {
+        if (jobs.isEmpty())
+            return;
+        while (! jobs.isEmpty()) {
+            SquareBoard task = nextJob();
+
+            /* need to kill other threads once a solution is found */
+        }
 
     }
 
